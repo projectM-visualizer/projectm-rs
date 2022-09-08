ba78e9f6b838213dcf11e9473395a2dcf1aa55fd
Blaquewithaq <blaquewithaq@gmail.com>
2022-09-08 10:20:33 -0500

    updated to build based on os

--- build.rs
+++ build.rs
@@ -2,42 +2,43 @@ use std::env;
 
 fn main() {
 
-  #[cfg(feature = "windows-x64")]
+  #[cfg(target_os = "windows")]
   let dst = cmake::build("projectm");
   
-  #[cfg(feature = "linux-x64")]
+  #[cfg(target_os = "linux")]
   let dst = cmake::build("projectm");
 
-  #[cfg(feature = "macos-x64")]
+  #[cfg(target_os = "ios")]
   let dst = cmake::build("projectm");
 
-  #[cfg(feature = "emscripten-x32")]
+  #[cfg(target_os = "emscripten")]
   let dst = cmake::build("projectm");
 
   println!("cargo:rustc-link-search=native={}/lib", dst.display());
+  // println!("cargo:rustc-link-arg=-sMIN_WEBGL_VERSION=2 -sMAX_WEBGL_VERSION=2");
 
-  #[cfg(feature = "windows-x64")]
+  #[cfg(target_os = "windows")]
   if Ok("release".to_owned()) == env::var("PROFILE") {
     println!("cargo:rustc-link-lib=dylib=projectM");
   } else {
     println!("cargo:rustc-link-lib=dylib=projectMd");
   }
 
-  #[cfg(feature = "linux-x64")]
+  #[cfg(target_os = "linux")]
   if Ok("release".to_owned()) == env::var("PROFILE") {
     println!("cargo:rustc-link-lib=dylib=projectM");
   } else {
     println!("cargo:rustc-link-lib=dylib=projectMd");
   }
 
-  #[cfg(feature = "macos-x64")]
+  #[cfg(target_os = "ios")]
   if Ok("release".to_owned()) == env::var("PROFILE") {
     println!("cargo:rustc-link-lib=dylib=projectM");
   } else {
     println!("cargo:rustc-link-lib=dylib=projectMd");
   }
   
-  #[cfg(feature = "emscripten-x32")]
+  #[cfg(target_os = "emscripten")]
   if Ok("release".to_owned()) == env::var("PROFILE") {
     println!("cargo:rustc-link-lib=static=projectM");
   } else {
