99143b239064c61cf953abe28856169f5c6e8f77
Blaquewithaq <blaquewithaq@gmail.com>
2022-12-15 21:30:52 -0600

    More safe-wrappers

--- src/lib.rs
+++ src/lib.rs
@@ -36,11 +36,13 @@
 //! ```
 //! 
 
+#![allow(non_camel_case_types)]
 
 extern crate libc;
 extern crate projectm_sys as ffi;
 
 use std::ffi::CStr;
+
 use libc::{c_int, strncpy};
 
 
@@ -68,6 +70,26 @@ pub struct Settings {
     pub easter_egg: f32,
 }
 pub type settings = Settings;
+
+pub type projectm_settings = ffi::projectm_settings_s;
+
+pub type preset_switch_requested_event = Option<fn(is_hard_cut: bool, user_data: *mut ())>;
+pub type preset_switch_failed_event = Option<fn(filename: &String, message: &String, user_data: *mut ())>;
+
+pub const MONO: projectm_channels = 1;
+pub const STEREO: projectm_channels = 2;
+pub type projectm_channels = u32;
+
+pub const TOUCH_TYPE_RANDOM: projectm_touch_type = 0;
+pub const TOUCH_TYPE_CIRCLE: projectm_touch_type = 1;
+pub const TOUCH_TYPE_RADIAL_BLOB: projectm_touch_type = 2;
+pub const TOUCH_TYPE_BLOB2: projectm_touch_type = 3;
+pub const TOUCH_TYPE_BLOB3: projectm_touch_type = 4;
+pub const TOUCH_TYPE_DERIVATIVE_LINE: projectm_touch_type = 5;
+pub const TOUCH_TYPE_BLOB5: projectm_touch_type = 6;
+pub const TOUCH_TYPE_LINE: projectm_touch_type = 7;
+pub const TOUCH_TYPE_DOUBLE_LINE: projectm_touch_type = 8;
+pub type projectm_touch_type = u32;
 
 impl projectm {
     pub fn create(settings: &settings) -> *mut ffi::projectm {
@@ -112,28 +134,6 @@ impl projectm {
         let projectm_settings_buf = unsafe { ffi::projectm_get_settings(instance) };
         let projectm_settings_ref = unsafe{ projectm_settings_buf.as_ref().unwrap() };
             
-        pub fn get_texture_path(instance: projectm_handle) -> String {
-            let texture_path_buf = unsafe { ffi::projectm_get_texture_path(instance) };
-            let texture_path_str = unsafe { CStr::from_ptr(texture_path_buf) };
-            let texture_path_str_slice = texture_path_str.to_str().unwrap();
-            let texture_path = texture_path_str_slice.to_owned();
-    
-            unsafe { ffi::projectm_free_string(texture_path_buf) };
-    
-            return texture_path;
-        }
-    
-        pub fn get_data_path(instance: projectm_handle) -> String {
-            let data_path_buf = unsafe { ffi::projectm_get_data_path(instance) };
-            let data_path_str = unsafe { CStr::from_ptr(data_path_buf) };
-            let data_path_str_slice = data_path_str.to_str().unwrap();
-            let data_path = data_path_str_slice.to_owned();
-    
-            unsafe { ffi::projectm_free_string(data_path_buf) };
-    
-            return data_path;
-        }
-
         let projectm_settings = Settings {
             mesh_x: projectm_settings_ref.mesh_x as u32,
             mesh_y: projectm_settings_ref.mesh_y as u32,
@@ -141,8 +141,8 @@ impl projectm {
             texture_size: projectm_settings_ref.texture_size as u32,
             window_width: projectm_settings_ref.window_width as u32,
             window_height: projectm_settings_ref.window_height as u32,
-            texture_path: get_texture_path(instance),
-            data_path: get_data_path(instance),
+            texture_path: projectm::get_texture_path(instance),
+            data_path: projectm::get_data_path(instance),
             preset_duration: projectm_settings_ref.preset_duration,
             soft_cut_duration: projectm_settings_ref.soft_cut_duration,
             hard_cut_duration: projectm_settings_ref.hard_cut_duration,
@@ -156,38 +156,31 @@ impl projectm {
         return projectm_settings;
     }
 
-    // pub fn write_config(config_file: String, settings: Settings) {
-    //     let projectm_settings = ffi::projectm_settings {
-    //         mesh_x: settings.mesh_x as c_int,
-    //         mesh_y: settings.mesh_y as c_int,
-    //         fps: settings.fps as c_int,
-    //         texture_size: settings.texture_size as c_int,
-    //         window_width: settings.window_width as c_int,
-    //         window_height: settings.window_height as c_int,
-    //         preset_path: unsafe { ffi::projectm_alloc_string((settings.preset_path.len() + 1).try_into().unwrap()) },
-    //         texture_path: unsafe { ffi::projectm_alloc_string((settings.texture_path.len() + 1).try_into().unwrap()) },
-    //         data_path: unsafe { ffi::projectm_alloc_string((settings.data_path.len() + 1).try_into().unwrap()) },
-    //         preset_duration: settings.preset_duration,
-    //         soft_cut_duration: settings.soft_cut_duration,
-    //         hard_cut_duration: settings.hard_cut_duration,
-    //         hard_cut_enabled: settings.hard_cut_enabled,
-    //         hard_cut_sensitivity: settings.hard_cut_sensitivity,
-    //         beat_sensitivity: settings.beat_sensitivity,
-    //         aspect_correction: settings.aspect_correction,
-    //         easter_egg: settings.easter_egg,
-    //         shuffle_enabled: settings.shuffle_enabled,
-    //         soft_cut_ratings_enabled: settings.soft_cut_ratings_enabled,
-    //     };
+    // -----------------
 
-    //     unsafe {
-    //         strncpy(projectm_settings.preset_path, settings.preset_path.as_bytes().as_ptr() as *mut i8, settings.preset_path.len());
-    //         strncpy(projectm_settings.texture_path, settings.texture_path.as_bytes().as_ptr() as *mut i8, settings.texture_path.len());
-    //         strncpy(projectm_settings.data_path, settings.data_path.as_bytes().as_ptr() as *mut i8, settings.data_path.len());
-    //     }
+    pub fn load_preset_file(instance: projectm_handle, filename: &String, smooth_transition: bool) {
+        unsafe { ffi::projectm_load_preset_file(instance, filename.as_bytes().as_ptr() as *mut i8, smooth_transition) };
+    }
 
-    //     let projectm_settings_ptr 
-    //     unsafe { ffi::projectm_write_config(config_file.as_bytes().as_ptr() as *mut i8, projectm_settings) };
+    pub fn load_preset_data(instance: projectm_handle, data: &String, smooth_transition: bool) {
+        unsafe { ffi::projectm_load_preset_data(instance, data.as_bytes().as_ptr() as *mut i8, smooth_transition) };
+    }
+
+    // pub fn set_preset_switch_requested_event_callback(instance: projectm_handle, callback: preset_switch_requested_event, user_data: *mut ()) {
+    //     unsafe { ffi::projectm_set_preset_switch_requested_event_callback(instance, callback, user_data as *mut ::std::os::raw::c_void) };
     // }
+
+    // pub fn set_preset_switch_failed_event_callback(instance: projectm_handle, callback: preset_switch_failed_event, user_data: *mut ()) {
+    //     unsafe { ffi::projectm_set_preset_switch_failed_event_callback(instance, callback, user_data as *mut ::std::os::raw::c_void) };
+    // }
+
+    pub fn is_preset_locked(instance: projectm_handle) -> bool {
+        return unsafe { ffi::projectm_is_preset_locked(instance) };
+    }
+
+    pub fn lock_preset(instance: projectm_handle, lock: bool) {
+        unsafe { ffi::projectm_lock_preset(instance, lock) };
+    }
 
     // -----------------
 
@@ -357,6 +350,24 @@ impl projectm {
     
     // -----------------
 
+    pub fn touch(instance: projectm_handle, x: f32, y: f32, pressure: i32, touch_type: projectm_touch_type) {
+        unsafe { ffi::projectm_touch(instance, x, y, pressure, touch_type) };
+    }
+
+    pub fn touch_drag(instance: projectm_handle, x: f32, y: f32, pressure: i32) {
+        unsafe { ffi::projectm_touch_drag(instance, x, y, pressure) };
+    }
+
+    pub fn touch_destroy(instance: projectm_handle, x: f32, y: f32) {
+        unsafe { ffi::projectm_touch_destroy(instance, x, y) };
+    }
+
+    pub fn touch_destroy_all(instance: projectm_handle) {
+        unsafe { ffi::projectm_touch_destroy_all(instance) };
+    }
+
+    // -----------------
+
     pub fn pcm_get_max_samples() -> u32 {
         return unsafe { ffi::projectm_pcm_get_max_samples() };
     }
@@ -365,27 +376,27 @@ impl projectm {
         instance: projectm_handle,
         samples: *const f32,
         count: u32,
-        // channels: projectm_channels, Fix channels
+        channels: projectm_channels,
     ) {
-        unsafe { ffi::projectm_pcm_add_float(instance, samples, count, 2) }
+        unsafe { ffi::projectm_pcm_add_float(instance, samples, count, channels) }
     }
 
     pub fn pcm_add_int16(
         instance: projectm_handle,
         samples: *const i16,
         count: u32,
-        // channels: projectm_channels, Fix channels
+        channels: projectm_channels,
     ) {
-        unsafe { ffi::projectm_pcm_add_int16(instance, samples, count, 2) }
+        unsafe { ffi::projectm_pcm_add_int16(instance, samples, count, channels) }
     }
 
     pub fn pcm_add_uint8(
         instance: projectm_handle,
         samples: *const u8,
         count: u32,
-        // channels: projectm_channels, Fix channels
+        channels: projectm_channels,
     ) {
-        unsafe { ffi::projectm_pcm_add_uint8(instance, samples, count, 2) }
+        unsafe { ffi::projectm_pcm_add_uint8(instance, samples, count, channels) }
     }
 
     // -----------------
